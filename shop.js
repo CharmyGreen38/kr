08a01a130240a95e47189e0124e1ba79b45ee0e2
Екатерина <kd1029384756@mail.ru>
2024-12-11 10:45:44 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -20,43 +20,47 @@ const content = [
     {
       "vendor_code": "4",
       "description": "Ленинградские математические олимпиады 1961-1991",
-      "price": 400,
+      "price": 666,
       "img": "https://biblio.mccme.ru/sites/default/files/books/lmo-obl.jpg"
     }
     ,
     {
       "vendor_code": "5",
       "description": "XIX––XX Турниры математических боёв им. А. П. Савина",
-      "price": 400,
+      "price": 345,
       "img": "https://biblio.mccme.ru/sites/default/files/books/gribalko-obl.jpg"
     },
     {
       "vendor_code": "6",
       "description": "Геометрия (4-е, исправленное)",
-      "price": 400,
+      "price": 500,
       "img": "https://biblio.mccme.ru/sites/default/files/books/975-5-4439-1440-4.jpg"
-    },
+    }
 
   ];
   
+
+function sort(){
+    let price=document.getElementById("price")
+    if(price.checked)
+      {document.getElementById('node_for_insert').innerHTML = '';
+       content.sort((a, b) => a.price - b.price);
+       getResponce();}
+}
+
+
   async function getResponce() {
-    let key
-    for (key in content) {
-        console.log(content[key].id, content[key].title)
-        console.log(content[key])
-    }
 
     let node_for_insert = document.getElementById("node_for_insert")
     for (key in content) {
         node_for_insert.innerHTML += `
-        <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
+        <li style="width: 210px" class="d-flex flex-column border bg-body">
         <img style="width: 180px" class="align-self-center" src=${content[key].img}>
-        <h5 class="card-title">${content[key].title}</h5>
         <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
-        <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
-        <p class="card-text" >Заказать <input class="w-25" type="number" name="amount" value="0"></p>
+        <input type="hidden" name="vendor_code" value=${content[key].vendor_code}>
+        <p class="card-text" >Заказать <input type="number" name="amount" value="0"></p>
         </li>
-                `
+        `
     }
 }
-getResponce()
+getResponce();
